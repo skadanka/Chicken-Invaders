3abeec3b2d234ce8e8353aed69c1a8c17f378660
Eden <rylynn2@gmail.com>
2023-04-03 21:45:39 +0300

    ships design is set

---
+++ Chicken-Invaders-main/menu.js
@@ -0,0 +1,235 @@
+const getAccounts = function() {
+    return JSON.parse(localStorage.getItem("Accounts"));
+}
+
+Storage.prototype.setAccount = function(username, account) {
+    const accounts = getAccounts();
+    accounts.push(account);
+    this.setItem("Accounts", JSON.stringify(accounts));
+}
+
+Storage.prototype.getAccount = function(username){
+    const findAccount = (account) => {
+        return account.username == username;
+    } ;
+    
+    const accounts = getAccounts();
+    const foundAccount = accounts.find(findAccount);
+    if(foundAccount){
+        return foundAccount;
+    }else{
+        return null;
+    }
+}
+
+
+
+const Connect = () => {
+    const username = document.getElementById("connect-username");
+    const password = document.getElementById("connect-password");
+    const msg = document.getElementById("login-msg");
+
+    var span=""
+
+    var account = localStorage.getAccount(username.value);
+    if(!account || account.password != password.value){
+        span = "<span style='color: red;'>Username or Passwords are incorrect</span>"
+        msg.innerHTML = "" + span;
+        return;
+    }
+
+    msg.innerHTML = "";
+    configureToggle();
+    loginToggle();
+}
+
+const Register = () => {
+    acceptForm = true;
+
+    check_username();
+    check_password();
+    verify_password();
+    check_firstName();
+    check_lastname();
+    check_email();
+
+    if (!acceptForm) {
+        return false;
+    }
+
+    var username = document.querySelector('#username-input');
+    var password1 = document.querySelector('#password1-input');
+    var firstName = document.querySelector('#first-name-input');
+    var lastName = document.querySelector('#last-name-input');
+    var birthdate = document.querySelector('#MoveInDate');
+    var email = document.querySelector('#email-input');
+
+    
+    const account = {
+        username: username.value,
+        password: password1.value,
+        firstname: firstName.value,
+        lastname: lastName.value,
+        birthdate: birthdate.value,
+        email: email.value
+    }
+    
+    localStorage.setAccount(username.value, account);
+
+    // localStorage.setItem(account.username, JSON.stringify(account));
+    loginToggle();
+    signInToggle();
+    return false;
+}
+
+var check_email = () => {
+    const emailIn = document.querySelector("#email-input");
+    const emailLb = document.querySelector("#email-label");
+
+    var span = "";
+
+    
+}
+
+var check_username = () => {
+    const usernameIn = document.querySelector("#username-input");
+    const usernameLb = document.querySelector("#username-label");
+
+    var span = ""
+
+    if (usernameIn.value.length < 5) {
+        var span = "<span style='color: red;'>Please Insert Valid Username</span>"
+        acceptForm = acceptForm && false;
+    }else if(localStorage.getItem(usernameIn.value)){
+        var span = "<span style='color: red;'>Username already Taken!</span>"
+        acceptForm = acceptForm && false;
+    }
+    usernameLb.innerHTML = "Username: " + span;
+}
+
+var check_password = () => {
+    const passwordIn = document.querySelector("#password1-input");
+    const passwordLb = document.querySelector("#password1-label");
+
+    var span = "";
+
+    if (passwordIn.value.length == 0) {
+        span = "<span style='color: red;'>Password must contain atleast 8 charcters</span>"
+        acceptForm = acceptForm && false;
+    } else if (!passwordIn.value.match("\\d") || !passwordIn.value.match("[a-zA-Z]")) {
+        span = "<span style='color:red;'>Password must contain Digits and Letters</span>"
+        acceptForm = acceptForm && false;
+    }
+
+    passwordLb.innerHTML = "Password: " + span;
+}
+
+function configure(){
+    var color = document.getElementById("spaceship-color").value;
+    var timer = document.getElementById("timer-setup").value;
+
+    setSpaceshipColor(color);
+    setTimer(timer)
+
+    configureToggle();
+    menuToggle(true);
+}
+
+var verify_password = () => {
+    const passwordInVerify = document.querySelector("#password2-input");
+    const passwordVerifyLb = document.querySelector("#password2-label");
+    const passwordIn = document.querySelector("#password1-input");
+
+    var span = "";
+
+    if (passwordInVerify.value != passwordIn.value) {
+        span = "<span style='color: red;'>Password doesn't match</span>"
+        acceptForm = acceptForm && false;
+    }
+    passwordVerifyLb.innerHTML = "Verify Password: " + span;
+};
+
+var check_firstName = () => {
+
+    const firstNameIn = document.querySelector("#first-name-input");
+    const firstNameLb = document.querySelector("#first-name-label");
+    var span = "";
+
+    if (firstNameIn.value.match("\\d")) {
+        span = "<span style='color:red;'>First name must contain only letters</span>"
+        acceptForm = acceptForm && false;
+    }
+
+    firstNameLb.innerHTML = "First Name: " + span;
+}
+
+var check_lastname = () => {
+    const lastnameIn = document.querySelector("#last-name-input");
+    const lastnameLb = document.querySelector("#last-name-label");
+    var span = "";
+
+    if (lastnameIn.value.match("\\d")) {
+        span = "<span style='color:red;'>Last name must contain only letters</span>"
+        acceptForm = acceptForm && false;
+    }
+    lastnameLb.innerHTML = "Last Name: " + span;
+}
+
+
+function menuToggle(state) {
+    var main_menu = document.getElementById("main-menu");
+    if(state){
+        main_menu.style.display = "block";
+        return;
+    }
+    if (main_menu.style.display === "none") {
+        main_menu.style.display = "block";
+    } else {
+        main_menu.style.display = "none";
+    }
+}
+
+function signInToggle() {
+    var signIn = document.getElementById("sign-in-screen");
+    if (signIn.style.display === "none") {
+        signIn.style.display = "block";
+    } else {
+        signIn.style.display = "none";
+    }
+}
+
+function loginToggle(){
+    var login = document.getElementById("login-screen");
+    if (login.style.display === "none") {
+        login.style.display = "block";
+    } else {
+        login.style.display = "none";
+    }
+}
+
+function aboutToggle(){
+    var about = document.getElementById("about-modal");
+    if (about.style.display === "none") {
+        about.style.display = "block";
+    } else {
+        about.style.display = "none";
+    }
+}
+
+
+function configureToggle(){
+    var about = document.getElementById("configure");
+    if (about.style.display === "none") {
+        about.style.display = "block";
+
+        var keyConfig = document.getElementById("fire-input")
+        keyConfig.addEventListener(
+            "keyup",
+            (event) => {
+                keyConfig.value = event.code;
+            }
+        )
+    } else {
+        about.style.display = "none";
+    }
+}
