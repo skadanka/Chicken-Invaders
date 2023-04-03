c3f11042fc77447b1b6033b6a95d8bb9b6a0674a
Eden <rylynn2@gmail.com>
2023-04-03 13:55:45 +0300

    working space and spaceship, need to solve the problems occuring using 2 java scripts files with the same main, can't clear the objects and redraw. but got cool effect overall, in addition need to add enemeis, redesign the code structure for optimazed creation of stars and clears

---
+++ spaceship.js
@@ -0,0 +1,66 @@
+const trailer = document.getElementById("trailer");
+var spaceship = {cx: 50, cy : 50}
+
+var battle;
+var contextBattle;
+
+var shoots = [];
+
+$(document).ready(function() {
+    battle = document.getElementById("battle");
+    // battle.width = window.innerWidth / 3;
+    // battle.height = window.innerHeight;
+    contextBattle = battle.getContext("2d");
+    battle.addEventListener("mousedown", function(e){
+        fire(e);
+    });
+
+    intervalTimer = setInterval(main, 1);
+})
+
+const drawSpaceship  = (cx, cy) => {
+    contextBattle.arc(cx, spaceship.cy, 10, 0, Math.PI*2);
+    contextBattle.fillStyle = "blue";
+    contextBattle.fill();
+    contextBattle.stroke()
+}
+
+window.onmousemove = e => {
+    const x = e.clientX - trailer.offsetWidth / 2, 
+          y = e.clientY - trailer.offsetHeight / 2;
+    
+    if(x > window.innerWidth / 3 - trailer.offsetWidth || x < 0 + trailer.offsetWidth/2){
+        return 
+    }
+    const keyframes = {
+            transform: `translate(${x}px)`
+        }
+
+    trailer.animate(keyframes, 
+        {
+            duration: 800,
+            fill: "forwards",
+        });
+    
+}
+
+function fire(event){
+    const rect = battle.getBoundingClientRect();
+    const tr = trailer.getBoundingClientRect();
+    const x = tr.x + trailer.offsetWidth/ 2, y = tr.y;
+    shoots.push({x: x, y: y});
+}
+
+
+function updateFirePositions(){
+    shoots.forEach(shoot => {       
+        shoot.y = shoot.y - 1;
+        contextBattle.beginPath();
+        contextBattle.fillStyle = "yellow";
+        contextBattle.arc(shoot.x, shoot.y, 4, 0, Math.PI*2);
+        contextBattle.fill();
+        contextBattle.stroke();
+    });
+}
+
+
